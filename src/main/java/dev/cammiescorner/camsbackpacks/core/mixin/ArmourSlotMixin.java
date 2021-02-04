c645c00c431f0a6b6fed2da73b848e0c119aa9be
CammiePone <inazuma261@gmail.com>
2021-02-04 14:03:06 -0500

    Added the only bug so far

--- src/main/java/dev/cammiescorner/camsbackpacks/core/mixin/ArmourSlotMixin.java
+++ src/main/java/dev/cammiescorner/camsbackpacks/core/mixin/ArmourSlotMixin.java
@@ -1,7 +1,6 @@
 package dev.cammiescorner.camsbackpacks.core.mixin;
 
 import dev.cammiescorner.camsbackpacks.common.items.BackpackItem;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.slot.Slot;
@@ -11,14 +10,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net/minecraft/screen/PlayerScreenHandler$1")
-public abstract class PlayerScreenHandlerMixin extends Slot
+public abstract class ArmourSlotMixin extends Slot
 {
-	public PlayerScreenHandlerMixin(Inventory inventory, int index, int x, int y) { super(inventory, index, x, y); }
+	public ArmourSlotMixin(Inventory inventory, int index, int x, int y) { super(inventory, index, x, y); }
 
 	@Inject(method = "canTakeItems(Lnet/minecraft/entity/player/PlayerEntity;)Z", at = @At("HEAD"), cancellable = true)
 	public void canTakeItems(PlayerEntity playerEntity, CallbackInfoReturnable<Boolean> info)
 	{
-		if(playerEntity.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof BackpackItem)
+		if(getStack().getItem() instanceof BackpackItem)
 		{
 			info.setReturnValue(false);
 		}
