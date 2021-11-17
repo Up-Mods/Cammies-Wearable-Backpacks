eb327cff730440662a0d10a15e6845fbe02aa0fe
CammiePone <inazuma261@gmail.com>
2021-11-16 23:14:33 -0500

    posm

--- src/main/java/dev/cammiescorner/camsbackpacks/client/screen/BackpackScreen.java
+++ src/main/java/dev/cammiescorner/camsbackpacks/client/screen/BackpackScreen.java
@@ -5,6 +5,7 @@ import dev.cammiescorner.camsbackpacks.CamsBackpacks;
 import dev.cammiescorner.camsbackpacks.common.network.EquipBackpackPacket;
 import dev.cammiescorner.camsbackpacks.common.screen.BackpackScreenHandler;
 import dev.cammiescorner.camsbackpacks.core.util.BackpackHelper;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
@@ -14,6 +15,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
@@ -25,12 +27,14 @@ public class BackpackScreen extends AbstractInventoryScreen<BackpackScreenHandle
 	protected PlayerEntity player;
 	protected int craftingX;
 	protected int craftingY;
+	protected int playerNameX;
+	protected int playerNameY;
 
 	public BackpackScreen(BackpackScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
 		super(screenHandler, playerInventory, text);
 		this.playerInventory = playerInventory;
 		this.backgroundWidth = 322;
-		this.backgroundHeight = 190;
+		this.backgroundHeight = 220;
 		this.player = playerInventory.player;
 	}
 
@@ -40,14 +44,27 @@ public class BackpackScreen extends AbstractInventoryScreen<BackpackScreenHandle
 		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 		RenderSystem.setShaderTexture(0, TEXTURE);
 		int x = (width - 322) / 2;
-		int y = (height - 190) / 2;
-		DrawableHelper.drawTexture(matrices, x, y, 0, 0, 0, 322, 190, 190, 322);
+		int y = (height - 220) / 2;
+		DrawableHelper.drawTexture(matrices, x, y, 0, 0, 0, 322, 190, 220, 322);
+		DrawableHelper.drawTexture(matrices, x + 1, y - 1, 0, 60, 190, 60, 30, 220, 322);
+
+		itemRenderer.renderInGui(getPlayerHead(), x + 8, y + 6);
+		itemRenderer.renderInGui(getBackpack(), x + 38, y + 6);
 	}
 
 	@Override
 	protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
 		super.drawForeground(matrices, mouseX, mouseY);
 		textRenderer.draw(matrices, new TranslatableText("container.crafting"), craftingX, craftingY, 4210752);
+
+		matrices.push();
+		String name = player.getEntityName();
+		float scale = Math.min(1F, 1 / (name.length() / 11F));
+		float translate = Math.max(0F, name.length() - 11);
+		matrices.scale(scale, scale, 1);
+		matrices.translate(translate, translate * 4, 0);
+		textRenderer.draw(matrices, player.getName(), playerNameX, playerNameY, 4210752);
+		matrices.pop();
 	}
 
 	@Override
@@ -84,7 +101,9 @@ public class BackpackScreen extends AbstractInventoryScreen<BackpackScreenHandle
 		playerInventoryTitleY = 96;
 		craftingX = 255;
 		craftingY = 34;
-		equipButton = addDrawableChild(new ButtonWidget(width / 2 + 86, height / 2 + 58, 68, 20, new TranslatableText(handler.isBlockEntity ? "container.camsbackpacks.equip" : "container.camsbackpacks.upequip"), this::doButtonShit));
+		playerNameX = 8;
+		playerNameY = 38;
+		equipButton = addDrawableChild(new ButtonWidget(width / 2 + 86, height / 2 + 48, 68, 20, new TranslatableText(handler.isBlockEntity ? "container.camsbackpacks.equip" : "container.camsbackpacks.upequip"), this::doButtonShit));
 
 		if(handler.isBlockEntity)
 			drawStatusEffects = false;
@@ -100,5 +119,16 @@ public class BackpackScreen extends AbstractInventoryScreen<BackpackScreenHandle
 			EquipBackpackPacket.send(true, handler.blockPos);
 		else if(!handler.isBlockEntity)
 			EquipBackpackPacket.send(false, handler.blockPos);
+	}
+
+	private ItemStack getPlayerHead() {
+		ItemStack head = new ItemStack(Blocks.PLAYER_HEAD);
+		// head.getOrCreateNbt().putString("SkullOwner", player.getEntityName());
+
+		return head;
+	}
+
+	private ItemStack getBackpack() {
+		return handler.isBlockEntity ? new ItemStack(player.world.getBlockState(handler.blockPos).getBlock()) : player.getEquippedStack(EquipmentSlot.CHEST);
 	}
 }
