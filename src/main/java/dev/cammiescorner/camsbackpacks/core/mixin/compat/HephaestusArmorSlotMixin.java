9c0e59173b9ef88ccfa942890c6e5bfa1f99e93e
Up <upcraftedlp@gmail.com>
2024-01-03 19:09:03 +0100

    add hephaestus compat

---
+++ src/main/java/dev/cammiescorner/camsbackpacks/core/mixin/compat/HephaestusArmorSlotMixin.java
@@ -0,0 +1,28 @@
+package dev.cammiescorner.camsbackpacks.core.mixin.compat;
+
+import dev.cammiescorner.camsbackpacks.common.items.BackpackItem;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import slimeknights.tconstruct.tables.menu.slot.ArmorSlot;
+
+@Mixin(ArmorSlot.class)
+public class HephaestusArmorSlotMixin extends Slot {
+
+    private HephaestusArmorSlotMixin(Container container, int slot, int x, int y) {
+        super(container, slot, x, y);
+        throw new UnsupportedOperationException();
+    }
+
+    @Inject(method = "mayPickup", at = @At("HEAD"), cancellable = true)
+    private void camsbackpacks$mayPickup(Player player, CallbackInfoReturnable<Boolean> cir) {
+        if(this.getItem().getItem() instanceof BackpackItem) {
+            cir.setReturnValue(false);
+        }
+    }
+
+}
