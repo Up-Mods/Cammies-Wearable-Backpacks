7e62be9090dbc54d9b39d34e1d1c002a68be1bbf
Up <upcraftedlp@gmail.com>
2024-01-12 08:37:34 +0100

    fix equipping armor items ignoring backpack

---
+++ Common/src/main/java/dev/cammiescorner/camsbackpacks/mixin/EquipableMixin.java
@@ -0,0 +1,18 @@
+package dev.cammiescorner.camsbackpacks.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.cammiescorner.camsbackpacks.item.BackpackItem;
+import net.minecraft.world.item.Equipable;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Equipable.class)
+public interface EquipableMixin {
+
+    @WrapOperation(method = "swapWithEquipmentSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;hasBindingCurse(Lnet/minecraft/world/item/ItemStack;)Z"))
+    default boolean camsbackpacks$hasBindingCurse(ItemStack stack, Operation<Boolean> original) {
+        return stack.getItem() instanceof BackpackItem || original.call(stack);
+    }
+}
