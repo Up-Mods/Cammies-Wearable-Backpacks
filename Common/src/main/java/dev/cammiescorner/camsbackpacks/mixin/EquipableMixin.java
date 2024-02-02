935a336ce73d906a0331c0f7746eb821bb23deae
Up <upcraftedlp@gmail.com>
2024-02-02 13:23:53 +0100

    fix mixin crashing the game on neoforge
    it still doesn't work but only affects creative mode so :shrug:

--- Common/src/main/java/dev/cammiescorner/camsbackpacks/mixin/EquipableMixin.java
+++ Common/src/main/java/dev/cammiescorner/camsbackpacks/mixin/EquipableMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public interface EquipableMixin {
 
     @WrapOperation(method = "swapWithEquipmentSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;hasBindingCurse(Lnet/minecraft/world/item/ItemStack;)Z"))
-    default boolean camsbackpacks$hasBindingCurse(ItemStack stack, Operation<Boolean> original) {
+    private boolean camsbackpacks$hasBindingCurse(ItemStack stack, Operation<Boolean> original) {
         return stack.getItem() instanceof BackpackItem || original.call(stack);
     }
 }
