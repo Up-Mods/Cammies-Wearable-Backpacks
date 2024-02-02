95d04ca87090b05030fbde9bebf0c077008321b4
Up <upcraftedlp@gmail.com>
2024-02-02 12:59:35 +0100

    update EnchantmentHelper mixin

--- Common/src/main/java/dev/cammiescorner/camsbackpacks/mixin/EnchantmentHelperMixin.java
+++ Common/src/main/java/dev/cammiescorner/camsbackpacks/mixin/EnchantmentHelperMixin.java
@@ -1,5 +1,6 @@
 package dev.cammiescorner.camsbackpacks.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.cammiescorner.camsbackpacks.item.BackpackItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
@@ -7,16 +8,16 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin {
 
-    @Inject(method = "getItemEnchantmentLevel", at = @At("HEAD"), cancellable = true)
-    private static void getItemEnchantmentLevelMixin(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        if (enchantment == Enchantments.BINDING_CURSE && stack.getItem() instanceof BackpackItem) {
-            cir.setReturnValue(1);
+    @ModifyReturnValue(method = "getItemEnchantmentLevel", at = @At("RETURN"))
+    private static int getItemEnchantmentLevelMixin(int original, Enchantment enchantment, ItemStack stack) {
+        if (original == 0 && enchantment == Enchantments.BINDING_CURSE && stack.getItem() instanceof BackpackItem) {
+            return 1;
         }
+
+        return original;
     }
 }
